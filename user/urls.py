9b291b0c016c8a1c687c32060e734383c87c6483
Shichao An <moleculeandrew@gmail.com>
2012-05-21 00:37:27 +0800

    2012/05/21 00:37

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Login view (Django bulit-in)              
-    url(r'^login/$', 'django.contrib.auth.views.login',{'template_name': 'login.html'}),
+    url(r'^login/$', 'django.contrib.auth.views.login',{'template_name': 'user_login.html'}),
     
     # Logout view  (Django bulit-in)
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
@@ -10,6 +10,11 @@ urlpatterns = patterns('',
     # User index page
     url(r'^$', 'user.views.index'),
     
+    # User password change page
+    url(r'^password/$', 'user.views.password'),
+    
     url(r'^config/$', 'user.views.config'),
+    
+
 
 )
--- user/views.py
+++ user/views.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+
+from django.template import RequestContext
+from django.template.loader import get_template
+
+
 from user.models import UserSPADE, UserConfig
+from django.contrib.auth.models import User
+# login_required decorator
 from django.contrib.auth.decorators import login_required
+# Form
+from django import forms
+# Built-in forms
+from django.contrib.auth.forms import PasswordChangeForm
 
 """
 index()
@@ -13,18 +25,53 @@ index()
 @login_required
 def index(request):
     username = request.user.username
-    html = "Hello %s, welcome to the Distributed Meeting Scheduler"%username
-    return HttpResponse(html)
+    #html = "Hello %s, welcome to the Distributed Meeting Scheduler"%username
+    #return HttpResponse(html)
+    return render_to_response('user.html',{'username':username})
+
+
+
+"""
+password()
+
+# url: /user/password/
+# Page for user to change password
+
+"""
+@login_required
+def password(request):
+    user = request.user
+    if request.method == 'POST': # If the form has been submitted
+        form = PasswordChangeForm(user, request.POST) # A form bound to the POST data
+        if form.is_valid(): 
+            # Call PasswordChangeForm.save() to save new password into User
+            form.save()
+            return HttpResponseRedirect('/user/') # Redirect after POST
+    else:
+        form = PasswordChangeForm(user)
+
+    c = RequestContext(request, {'form': form})
+    t = get_template('user_password.html')
+    return HttpResponse(t.render(c))
+
+
+
+"""
+config()
+
+# url: /user/config/
+# Configuration page for users
+
+"""
 
 @login_required
 def config(request):
-    #user = UserSPADE.objects.get(pk=1)
-    #username = user.user_name
+
     username = request.user.username
     id = request.user.id
-    html = "Hello World, " + username
     data = UserConfig.objects.get(pk=id)
-    html += "\n%s %s %s %s"%(data.msa_id, data.ca_id, data.pref_period, data.best_period)
-    return HttpResponse(html)
-
+    agenda = {'pref':data.pref_period,'best':data.best_period}
+    c = RequestContext(request, {'msa': data.msa_id,'ca':data.ca_id,'agenda':agenda})
+    t = get_template('user_config.html')
+    return HttpResponse(t.render(c))
 
